7daee9f4e7a85ae817b617aac1dbb9926e5d0c04
BOBI4515oo <80900941+BOBI4515oo@users.noreply.github.com>
2022-11-13 14:56:38 +0300

    0.3.4
    add new planets
    add new core
    bug fix

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+
+require('outpost');
+//require('campaign/serpulo');
---
+++ scripts/music.js
@@ -0,0 +1,5 @@
+
+const { ambientMusic : music } = Vars.control.sound;
+
+music.add(loadMusic('mysterious-world'));
+music.add(loadMusic('impending-doom'));
